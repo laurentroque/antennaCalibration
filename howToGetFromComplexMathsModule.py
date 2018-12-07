a0387939f984b35f237a631b989ee8647ca7cf1a
Ian Roque <ilvr2@ad.ra.phy.private.cam.ac.uk>
2018-12-07 11:30:28 +0000

    Commands to retrieve information from complexMaths.py

---
+++ howToGetFromComplexMathsModule.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec  7 11:26:45 2018
+
+@author: ilvr2
+"""
+
+import complexMaths
+print
+print complexMaths.mathsForT_cos
+
+print type(complexMaths.mathsForT_cos[3][1])
